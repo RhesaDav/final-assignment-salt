9ec73d7ff6fafc38b7a0ad072e798ddf5f2a3b43
asmon <oscarsolideo@gmail.com>
2022-01-15 00:16:53 +0700

    edit student v2

--- apps/frontend-sd/src/app/_service/kelas.service.ts
+++ apps/frontend-sd/src/app/_service/kelas.service.ts
@@ -3,21 +3,20 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { kelas } from '../_models/kelas';
-
-const port = environment.port
+// TESTER
+const port = environment.port;
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class KelasServices {
-
-  constructor(private http: HttpClient) { }
-  getAllClass():Observable<kelas[]>{
-    return this.http.get<kelas[]>(`${port}/api/class`)
+  constructor(private http: HttpClient) {}
+  getAllClass(): Observable<kelas[]> {
+    return this.http.get<kelas[]>(`${port}/api/class`);
   }
 
-  AddClass(data:any):Observable<any>{
-    console.log('data:', data)
-    return this.http.post<any>(`${port}/api/class`, data)
+  AddClass(data: any): Observable<any> {
+    console.log('data:', data);
+    return this.http.post<any>(`${port}/api/class`, data);
   }
 }
