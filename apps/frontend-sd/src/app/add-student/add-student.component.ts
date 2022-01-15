33c3f33b828f3c28879f4d23afa9719a16170747
asmon <oscarsolideo@gmail.com>
2022-01-15 14:12:04 +0700

    edit student v4

--- apps/frontend-sd/src/app/add-student/add-student.component.ts
+++ apps/frontend-sd/src/app/add-student/add-student.component.ts
@@ -1,71 +1,76 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { StudentService } from '../_service/student.service';
 
 @Component({
   selector: 'app-addstudent',
   templateUrl: './add-student.component.html',
-  styleUrls: ['./add-student.component.css']
+  styleUrls: ['./add-student.component.css'],
 })
 export class AddStudentComponent implements OnInit {
-
   form!: FormGroup;
-  genderType?:string;
+  genderType?: string;
   imageDisplay?: string | ArrayBuffer | undefined;
-  constructor(private  formbBuilder: FormBuilder, private studentService:StudentService) { }
+  constructor(
+    private formbBuilder: FormBuilder,
+    private studentService: StudentService
+  ) {}
 
   ngOnInit(): void {
-    this._formInit()
+    this._formInit();
   }
 
-  private _formInit(){
+  private _formInit() {
     this.form = this.formbBuilder.group({
-      first_name : ['', Validators.required],
-      last_name : ['', Validators.required],
-      gender : ['', Validators.required],
-      father_name : ['', Validators.required],
-      mother_name : ['', Validators.required],
-      date_of_birth : ['', Validators.required],
-      father_occupation : ['', Validators.required],
-      blood_group : ['', Validators.required],
-      religion : ['', Validators.required],
-      email : ['', Validators.required],
-      addmission_date : ['', Validators.required],
-      kelas : ['', Validators.required],
-      address : ['', Validators.required],
-      phone : ['', Validators.required],
+      first_name: ['', Validators.required],
+      last_name: ['', Validators.required],
+      gender: ['', Validators.required],
+      father_name: ['', Validators.required],
+      mother_name: ['', Validators.required],
+      date_of_birth: ['', Validators.required],
+      father_occupation: ['', Validators.required],
+      blood_group: ['', Validators.required],
+      religion: ['', Validators.required],
+      email: ['', Validators.required],
+      addmission_date: ['', Validators.required],
+      kelas: ['', Validators.required],
+      address: ['', Validators.required],
+      phone: ['', Validators.required],
       short_bio: ['', Validators.required],
-      image : ['', Validators.required],
-      year_academic : ['', Validators.required],
-    })
+      image: ['', Validators.required],
+      year_academic: ['', Validators.required],
+    });
   }
 
-
-  submitData(){
-
+  submitData() {
+    console.log('Student Form', this.studentForm);
     const studentData = new FormData();
-    Object.keys(this.studentForm).map((key)=>{
-      studentData.append(key, this.studentForm[key].value)
-    })
-     this.studentService.AddStudent(studentData).subscribe((res)=>{
-      console.log("Hasil : ", res)
-    })
+    Object.keys(this.studentForm).map((key) => {
+      studentData.append(key, this.studentForm[key].value);
+    });
+    this.studentService.AddStudent(studentData).subscribe((res) => {
+      console.log('Hasil : ', res);
+    });
   }
 
-  uploadImage(event:any){
-    const file = event.target.files[0]
-    if(file){
-      this.form.patchValue({image : file})
+  uploadImage(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      this.form.patchValue({ image: file });
       this.form.get('image')?.updateValueAndValidity();
       const fileReader = new FileReader();
-      fileReader.onload = ()=>{
+      fileReader.onload = () => {
         this.imageDisplay = fileReader.result;
-      }
-      fileReader.readAsDataURL(file)
+      };
+      fileReader.readAsDataURL(file);
     }
   }
-  get studentForm(){
+  get studentForm() {
     return this.form?.controls;
   }
 }
-
